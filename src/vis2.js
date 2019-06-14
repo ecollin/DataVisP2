dee33c1e1e6713d1c57a0436f5f62b790bfbac46
Enrique <enriquecollin99@gmail.com>
2019-06-14 17:32:35 -0400

    Finished vis2

--- src/vis2.js
+++ src/vis2.js
@@ -2,6 +2,7 @@ import {select} from 'd3-selection';
 import {scaleBand, scaleLinear, scaleOrdinal} from 'd3-scale';
 import {schemeSet2} from 'd3-scale-chromatic';
 import {axisBottom, axisLeft} from 'd3-axis';
+import {getRegion} from './utils.js';
 
 // Takes the original data--an array of objects where each entry is 
 // an olympian-- and converts into an object where each prop is a year
@@ -10,9 +11,23 @@ import {axisBottom, axisLeft} from 'd3-axis';
 
 const regions = ['Americas', 'Asia', 'Europe', 'Africa', 'Oceania'];
 
+// Below is list of years in which you'd expect olympics but there were none
+const badYears = ['1906', '1916', '1940', '1944'];
+
 function processData(data) {
+const temp =  data.reduce((acc, entry) => {
+    const year = entry['Year'];
+    if (!acc[year]) acc[year] = year;
+    return acc;
+  }, {});
   const medalists = data.filter((entry) => {
-    return entry['Medal'] !== 'NA';
+    return entry['Medal'] !== 'NA' && 
+    // Olympics were unofficial in 1906
+      entry['Year'] !== '1906' && 
+    // Olympics were cancelled in the years below
+      entry['Year'] !== '1916' &&
+      entry['Year'] !== '1940' &&
+      entry['Year'] !== '1944';
   });
  
   const regionData = medalists.reduce((acc, entry) => {
@@ -47,16 +62,6 @@ function processData(data) {
 }
 
 export function makeVis2(data) {
-
-const teams = data.reduce((acc, entry) => {
-  const team = entry['Team'];
-  if (!acc[team] && entry['Medal'] !== 'NA'){
-  acc[team] = team;
-}
-  return acc;
-}, {});
-console.log(teams);
- 
   const height = 500;
   const width = 500;
   const div = select('#app').append('div')
@@ -67,11 +72,11 @@ console.log(teams);
     .attr('width', width)
     .attr('height', height);
 
-  makeSlider(data, svg);
-  drawChart(data, svg);  
+  makeSlider(data);
+  drawChart(data);  
 }
 
-function drawChart(data, svg) {
+function drawChart(data) {
   const width = 500;
   const height = 500;
   const margin = {top: 50, left: 80, right: 50, bottom: 150};
@@ -82,11 +87,12 @@ function drawChart(data, svg) {
   // and that prop holds an obj with medal counts for the 5 regions
   // in that year: Asia, Europe, Americas, Oceania, and Africa
   const medalData = processData(data);
-  const year = 2008;//document.querySelector('.yearSlider').value;
+  const year = document.querySelector('.yearSlider').value;
   const regions = ['Americas', 'Asia', 'Europe', 'Africa', 'Oceania'];
 
   const yearlyVals = regions.reduce((acc, region) => {
     const newObj = {region, val: medalData[year][region]};
+    if (isNaN(newObj.val)) newObj.val = 0;
     acc.push(newObj); 
     return acc;
   }, []);
@@ -102,6 +108,7 @@ function drawChart(data, svg) {
     .domain(regions)
     .range(schemeSet2.slice(0, 5));
   
+  const svg = select('.medalChart');
   svg.selectAll('.bars')
     .data(yearlyVals)
     .enter()
@@ -137,7 +144,7 @@ function drawChart(data, svg) {
      .text('Percent of medals');
 }
 
-function makeSlider(data, svg) {
+function makeSlider(data) {
   const sliderDiv = select('.vis2')
     .append('div')
     .attr('class', 'sliderDiv');
@@ -159,209 +166,21 @@ function makeSlider(data, svg) {
     .attr('text-anchor', 'middle')	
     .text('Year: ' + range.min);
 
-  slider.on('input', e => {
+  slider.on('change', e => {
     const curr = Number(slider.property('value'));
-    if (curr < 1924 && (curr - range.min) % 4 !== 0) {
-      // Before 1924 there were only olympics ever 2 years b/c there were no winter
+    if (curr < 1992 && (curr - range.min) % 4 !== 0) {
+      // Before 1992 there were only olympics ever 2 years b/c there were no winter
       // olympics. So move down 2 years if in this range.  
       document.querySelector('.yearSlider').value = curr - 2;
     }
+    while (badYears.map(Number).includes(Number(slider.property('value')))) {
+      document.querySelector('.yearSlider').value = curr - 2;
+    }
     label.text('Year: ' + slider.property('value'))     
+    select('.medalChart').selectAll('*').remove();
+    makeVis2(data);
     
   });
 }
 
 
-function getRegion(country) {
-  const regions = {
-    'China': 'Asia',
-    'Greece': 'Europe',
-    'United States': 'Americas',
-    'Australia': 'Oceania',
-    'South Africa': 'Africa',
-    'A North American Team': 'Americas',
-    'Afghanistan': 'Asia',
-    'Algeria': 'Africa',
-    'Ali-Baba II': 'Europe',
-    'Amateur Athletic Association': 'Europe',
-    'Amstel Amsterdamn': 'Europe',
-    'Ancora': 'Europe',
-    'Angelita': 'Americas',
-    'Antwerpia V': 'Europe',
-    'Aphrodite': 'Americas', 
-    'Argentina': 'Americas',
-    'Argonaut Rowing Club': 'Americas',
-    'Armenia': 'Asia',
-    'Aschenbrodel': 'Europe',
-    'Atalanta Boat Club-1': 'Americas',
-    'Atalanta Boat Club-2': 'Americas', 
-    'Atlanta': 'Americas',
-    'Australasia': 'Oceania',
-    'Australia': 'Oceania',
-    'Australia-1': 'Oceania',
-    'Australia/Great Britain': 'Oceania',
-    'Austria': 'Europe',
-    'Austria-1': 'Europe',
-    'Austria-2': 'Europe',
-    'Azerbaijan': 'NA',
-    'BLO Polo Club, Rugby': 'Europe',
-    'Baby-1': 'Europe',
-    'Bagatelle Polo Club, Paris': 'Europe', 
-    'Bahamas': 'Americas',
-    'Bahrain': 'Asia',
-    'Ballerina IV': 'Europe',
-    'Barbados': 'Americas',
-    'Barion/Bari-2': 'Europe',
-    'Barrenjoey': 'Oceania',
-    'Beatrijs III-1': 'Europe',
-    'Belarus': 'Europe',
-    'Belgium': 'Europe',
-    'Belgium-1': 'Europe',
-    'Bem II': 'Europe',
-    'Bera': 'Europe',
-    'Berliner Ruderclub': 'Europe',
-    'Berliner Ruderverein von 1876-2': 'Europe',
-    'Bermuda': 'Americas',
-    'Bingo': 'Americas',
-    'Bissbi': 'Europe',
-    'Bluebottle': 'Europe',
-    'Bohemia': 'Europe', 
-    'Bohemia/Great Britain': 'Europe',
-    'Bona Fide': 'Europe',
-    'Bonaparte': 'Europe',
-    'Bonzo': 'Europe',
-    'Boreas-2': 'Europe', 
-    'Boston Archers': 'Americas',
-    'Botswana': 'Africa',
-    'Brazil': 'Americas',
-    'Brazil-1': 'Americas',
-    'Brazil-2': 'Americas',
-    'Brussels Swimming and Water Polo Club': 'Europe',
-    'Brynhild-2': 'Europe',
-    'Bucintoro Venezia': 'Europe',
-    'Bucintoro Venezia-1': 'Europe',
-    'Bulgaria': 'Europe',
-    'Buraddoo': 'Oceania',
-    'Burundi': 'Africa',
-    'Cambridge University Boat Club-2': 'Europe',
-    'Cameroon': 'Africa',
-    'Camille': 'Europe',
-    'Canada-1': 'Americas',
-    'Canada-2': 'Americas',
-    'Caprice': 'Americas', 
-    'Carabinier-15': 'Europe',
-    'Central Turnverein, Chicago': 'Americas',
-    'Century Boat Club-1': 'Americas',
-    'Cercle de l\'Aviron Roubaix-4': 'Europe',
-    'Chicago Athletic Association': 'Americas',
-    'Chicago Athletic Association-2': 'Americas',
-    'Chile': 'Americas',
-    'China':'Asia',
-    'China-1':'Asia',
-    'China-2':'Asia',
-    'China-3':'Asia',
-    'Chinese Taipei': 'Asia',
-    'Christian Brothers\' College-1': 'Americas',
-    'Chuckles': 'Europe',
-    'Cicely-1': 'Europe', 
-    'Cincinnati Archers': 'Americas',
-    'Clearwater': 'Europe', 
-    'Club Nautique de Lyon-2': 'Europe',
-    'Cobweb-1': 'Europe',
-    'Colombia': 'Americas',
-    'Comanche': 'Americas',
-    'Complex II': 'Americas',
-    'Cornwall': 'Europe',
-    'Costa Rica': 'Americas',
-    'Cote d\'Ivoire': 'Africa',
-    'Crabe II-1': 'Europe',
-    'Crabe II-4': 'Europe',
-    'Croatia': 'Euroep',
-    'Cuba': 'Americas',
-    'Cyprus': 'Europe',
-    'Czech Republic': 'Europe',
-    'Czech Republic-1': 'Europe',
-    'Czechoslovakia': 'Europe',
-    'Czechoslovakia-1': 'Europe',
-    'Denmark': 'Europe',
-    'Denmark-1': 'Europe',
-    'Denmark-2': 'Europe',
-    'Denmark/Sweden': 'Europe',
-    'Deutscher Schwimm Verband Berlin': 'Europe',
-    'Devon and Somerset Wanderers': 'Europe',
-    'Digby': 'Oceania',
-    'Djibouti': 'Africa',
-    'Djinn': 'Americas',
-    'Dominican Republic': 'Americas',
-    'Don Schufro': 'Europe',
-    'Dormy-1': 'Europe',
-    'East Germany': 'Europe',
-    'East Germany-1': 'Europe',
-    'East Germany-2': 'Europe',
-    'Ecuador': 'Americas',
-    'Edelweiss II-1': 'Europe',
-    'Egypt': 'Africa',
-    'Eleda': 'Europe',
-    'Elisabeth V': 'Europe',
-    'Elisabeth X': 'Europe',
-    'Elsie': 'Europe',
-    'Elvis Va': 'Europe',
-    'Emily': 'Europe',
-    'Encore': 'Europe',
-    'England': 'Europe',
-    'England-1': 'Europe',
-    'Eritrea': 'Africa',
-    'Erna Signe': 'Europe',
-    'Espadarte': 'Europe',
-    'Esterel-1': 'Europe',
-    'Estonia': 'Europe',
-    'Ethiopia': 'Africa',
-    'Ethnikos Gymnastikos Syllogos': 'Europe',
-    'Falcon IV': 'Oceania',
-    'Fantlet-7': 'Europe',
-    'Favorite Hammonia-3': 'Europe',
-    'Favorite-1': 'Europe',
-    'Femur-1': 'Europe',
-    'Fiji': 'Oceania',
-    'Finland': 'Europe', 
-    'Formosa': 'Americas',
-    'Fornebo': 'Europe',
-    'Foxhunters Hurlingham': 'Europe',
-    'France': 'Europe',
-    'France-1': 'Europe',
-    'France-2': 'Europe',
-    'France-3': 'Europe',
-    'France/Great Britain': 'Europe',
-    'Frankfurt Club': 'Europe',
-    'Frimousse': 'Europe',
-    'Gabon': 'Africa',
-    'Gallant': 'Americas',
-    'Gallia II': 'Europe',
-    'Galt Football Club': 'Americas',
-    'Gem': 'Americas',
-    'Gem IV': 'Americas',
-    'Georgia': 'Asia',
-    'Germania II': 'Europe',
-    'Germania Ruder Club, Hamburg-2': 'Europe',
-    'Germany': 'Europe',
-    'Germany-1': 'Europe',
-    'Germany-2': 'Europe',
-    'Ghana': 'Africa',
-    'Gitana-2': 'Europe',
-    'Great Britain': 'Europe',
-    'Great Britain-1': 'Europe',
-    'Great Britain-2': 'Europe',
-    'Great Britain-3': 'Europe',
-    'Great Britain/Germany': 'Europe',
-    'Greece': 'Europe',
-    'Greece-1': 'Europe',
-    'Greece-2': 'Europe',
-    'Grenada': 'Americas',
-    'Guatemala': 'Americas',
-    'Gustel X': 'Europe',
-    'Guyana': 'Americas'
-  };
-
-  if (!regions[country]) return 'NA';
-  return regions[country];
-}
